b9ae5e39b8979c1b6ac0a5e2e56c07425fb44e16
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-10-07 19:46:14 -0700

    First effort
    - install SCSS
    - pass data from form to parent component

---
+++ app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import TextForm from './TextForm';
+import './App.scss';
+
+class App extends Component {
+  printText(text) {
+    console.log('text', text);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__title">Text Analytics API Demo</h1>
+        <h3 className="App__textbox-label">Enter Phrase</h3>
+        <TextForm
+          printText={this.printText}
+        />
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        here
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/src/TextForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './TextForm.scss';
+
+class TextForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.props.printText(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input className="Text-form__input" type="text" value={this.state.value} onChange={this.handleChange} />
+          <div className="Text-form__analyze">
+            <input  type="submit" value="Submit" />
+          </div>
+        </form>
+        <p>hmmm</p>
+      </div>
+    );
+  }
+}
+
+export default TextForm;
